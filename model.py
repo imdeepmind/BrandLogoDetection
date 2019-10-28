0895cd3d9d58146804cd88652c121e6512aa9b61
Abhishek Chatterjee <abhishekchatterjeejit@gmail.com>
2019-10-28 19:50:24 +0530

    added web ui

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch 
+import torch.nn as nn
+
+class Network(nn.Module):
+    def __init__(self):
+        pass
+    
+    def forward(self, x):
+        pass
+    
+class Model:
+    def train(self, X, y):
+        pass
+    
+    def test(self, X, y):
+        pass
+    
+    def __init__(self):
+        pass
